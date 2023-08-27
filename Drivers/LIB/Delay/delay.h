7eb1e8b56ad7a04d3f8ba00dd5b36d0600e19d9a
Mohamed Abdelazez <72413579+abdelazez0@users.noreply.github.com>
2023-08-27 14:37:36 +0300

    Add files via upload

---
+++ Drivers/LIB/Delay/delay.h
@@ -0,0 +1,16 @@
+/*
+ * delay.h
+ *
+ *  Created on: Aug 19, 2023
+ *      Author: Mohamed Abdelazez
+ */
+
+#ifndef DELAY_H_
+#define DELAY_H_
+
+/* delay function */
+void _delay_ms(uint32 n);
+
+void _delay_us( uint32 n);
+
+#endif /* DELAY_H_ */
